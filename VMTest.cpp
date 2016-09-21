7db313e7bf722807b4b814122728e9e3c3ef80c1
Andreas Mecky <a.mecky@verband.creditreform.de>
2016-09-21 16:08:07 +0200

    first step at supporting methods in scripting

--- VMTest.cpp
+++ VMTest.cpp
@@ -10,10 +10,11 @@ TEST_CASE("VM_Basic", "[VM]") {
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);	
 	//char* txt = ds::file::loadTextFile("test.script");
 	char* txt = "R1 = R2 / 4.0;";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
-	ds::vm::parse(txt,ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	const ds::vm::Method& m = ctx->getMethod(SID("default"));
+	REQUIRE(1 == m.lines.size());
+	const ds::vm::Line& line = m.lines[0];
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_DIV);
 	delete ctx;
@@ -26,14 +27,14 @@ TEST_CASE("VM_Basic1", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0;";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
-	ds::vm::parse(txt, ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_NONE);
-	ds::vm::execute(ctx);
-	REQUIRE(4.0f == ctx->data[1].x);
+	ctx->execute();
+	REQUIRE(4.0f == ctx->getRegister(1).x);
 	delete ctx;
 	delete ds::gDefaultMemory;
 
@@ -43,14 +44,14 @@ TEST_CASE("VM_Basic2", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0 + 8.0;";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
-	ds::vm::parse(txt, ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_PLUS);
-	ds::vm::execute(ctx);
-	REQUIRE(12.0f == ctx->data[1].x);
+	ctx->execute();
+	REQUIRE(12.0f == ctx->getRegister(1).x);
 	delete ctx;
 	//delete[] txt;
 	delete ds::gDefaultMemory;
@@ -60,14 +61,14 @@ TEST_CASE("VM_Basic3", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0 + 8.0;\nR2 = R1 / 2.0;";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
-	ds::vm::parse(txt, ctx);
-	REQUIRE(2 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[1];
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	REQUIRE(2 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(1);
 	REQUIRE(2 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_DIV);
-	ds::vm::execute(ctx);
-	REQUIRE(6.0f == ctx->data[2].x);
+	ctx->execute();
+	REQUIRE(6.0f == ctx->getRegister(2).x);
 	delete ctx;
 	//delete[] txt;
 	delete ds::gDefaultMemory;
@@ -77,15 +78,15 @@ TEST_CASE("VM_Basic4", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0 + DT;";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
 	ctx->set(SID("DT"), v4(1, 2, 3, 4));
-	ds::vm::parse(txt, ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_PLUS);
-	ds::vm::execute(ctx);
-	REQUIRE(5.0f == ctx->data[1].x);
+	ctx->execute();
+	REQUIRE(5.0f == ctx->getRegister(1).x);
 	delete ctx;
 	//delete[] txt;
 	delete ds::gDefaultMemory;
@@ -95,15 +96,15 @@ TEST_CASE("VM_Basic5", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0 + SIN(DT);";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
 	ctx->set(SID("DT"), v4(HALF_PI));
-	ds::vm::parse(txt, ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_PLUS);
-	ds::vm::execute(ctx);
-	REQUIRE(5.0f == ctx->data[1].x);
+	ctx->execute();
+	REQUIRE(5.0f == ctx->getRegister(1).x);
 	delete ctx;
 	delete ds::gDefaultMemory;
 }
@@ -112,14 +113,14 @@ TEST_CASE("VM_Basic6", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0;\nR2 = 10.0;\nR3 = 1.0 + LRP(R1,R2,0.5);\n";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
-	ds::vm::parse(txt, ctx);
-	REQUIRE(3 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[2];
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	REQUIRE(3 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(2);
 	REQUIRE(3 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_PLUS);
-	ds::vm::execute(ctx);
-	REQUIRE(8.0f == ctx->data[3].x);
+	ctx->execute();
+	REQUIRE(8.0f == ctx->getRegister(3).x);
 	delete ctx;
 	delete ds::gDefaultMemory;
 }
@@ -128,16 +129,57 @@ TEST_CASE("VM_Basic7", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
 	char* txt = "R1 = 4.0 * SIN(DT);";
-	ds::vm::VMContext* ctx = new ds::vm::VMContext("Test");
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
 	ctx->registerVar(SID("DT"));
-	ds::vm::parse(txt, ctx);
-	REQUIRE(1 == ctx->lines.size());
-	const ds::vm::Line& line = ctx->lines[0];
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
 	REQUIRE(1 == line.register_index);
 	REQUIRE(line.operation == ds::vm::Operation::OP_MUL);
 	ctx->set(0, v4(HALF_PI));
-	ds::vm::execute(ctx);
-	REQUIRE(4.0f == ctx->data[1].x);
+	ctx->execute();
+	REQUIRE(4.0f == ctx->getRegister(1).x);
+	delete ctx;
+	delete ds::gDefaultMemory;
+}
+
+TEST_CASE("VM_Basic8", "[VM]") {
+	init_logger(LogTypes::LT_FILE, 0, 0);
+	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
+	char* txt = "R1 = 4.0 * COS(TWO_PI);";
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	REQUIRE(1 == ctx->numLines());
+	const ds::vm::Line& line = ctx->getLine(0);
+	REQUIRE(1 == line.register_index);
+	REQUIRE(line.operation == ds::vm::Operation::OP_MUL);
+	ctx->execute();
+	REQUIRE(4.0f == ctx->getRegister(1).x);
+	delete ctx;
+	delete ds::gDefaultMemory;
+}
+
+TEST_CASE("VM_Basic9", "[VM]") {
+	init_logger(LogTypes::LT_FILE, 0, 0);
+	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
+	char* txt = "R1 = 4.0 * SIN(TWO_PI);";
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	ctx->set(0, v4(HALF_PI));
+	ctx->execute();
+	REQUIRE(4.0f == ctx->getRegister(1).x);
+	delete ctx;
+	delete ds::gDefaultMemory;
+}
+
+TEST_CASE("VM_Function1", "[VM]") {
+	init_logger(LogTypes::LT_FILE, 0, 0);
+	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
+	char* txt = "function wiggle() {\nR1 = 4.0 + 8.0;\n}";
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	ctx->parse(txt);
+	ctx->execute();
+	REQUIRE(12.0f == ctx->getRegister(1).x);
 	delete ctx;
 	delete ds::gDefaultMemory;
 }
