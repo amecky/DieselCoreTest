c8bd5ef2f7e153cff700acf7a0e881d575715b91
Andreas Mecky <a.mecky@verband.creditreform.de>
2016-09-26 16:08:53 +0200

    more tests

--- VMTest.cpp
+++ VMTest.cpp
@@ -244,6 +244,19 @@ TEST_CASE("VM_Function4", "[VM]") {
 	delete ds::gDefaultMemory;
 }
 
+TEST_CASE("VM_Function5", "[VM]") {
+	init_logger(LogTypes::LT_FILE, 0, 0);
+	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
+	char* txt = "function wiggle {\nR1 = 4.0;\nR2 = TWN(0,1.0,2.0,0.5,1.0);\n}";
+	ds::vm::Script* ctx = new ds::vm::Script("Test");
+	bool ret = ctx->parse(txt);
+	REQUIRE(ret == false);
+	ctx->execute(SID("wiggle"));
+	REQUIRE(1.5f == ctx->getRegister(2).x);
+	delete ctx;
+	delete ds::gDefaultMemory;
+}
+
 TEST_CASE("VM_Basic11", "[VM]") {
 	init_logger(LogTypes::LT_FILE, 0, 0);
 	ds::gDefaultMemory = new ds::DefaultAllocator(64 * 1024 * 1024);
